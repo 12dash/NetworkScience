cb6e504e74a56cf53f4aa85134ebc593d1cbe358
12dash <soham.dandapath@gmail.com>
2021-04-04 18:18:20 +0800

    Tabs and splitting of Data

--- interface_new.py
+++ interface_new.py
@@ -53,17 +53,17 @@ def generate_year_graph(network, title="Years"):
 
     return plot
 
-def generate_giant_componnet(network, title = "Largest Giant Component"):
-    plot = figure(toolbar_location = None, x_range=Range1d(-11.1, 11.1), y_range=Range1d(-11.1, 11.1), title=str(title), plot_width=400, plot_height=400)
+def generate_giant_componnet(network, title = "Giant Components"):
+    HOVER_TOOLTIPS = [("Faculty", "@index")]
+    plot = figure(tooltips = HOVER_TOOLTIPS, toolbar_location = None, x_range=Range1d(-11.1, 11.1), y_range=Range1d(-11.1, 11.1), title=str(title), plot_width=400, plot_height=400)
     network_graph = from_networkx(network, nx.spring_layout, scale=10, center=(0, 0))
     network_graph.node_renderer.glyph = Circle()
     network_graph.edge_renderer.glyph = MultiLine(line_alpha=0.5, line_width=1)
     plot.renderers.append(network_graph)
     return plot
 
-def generate_tab_year(graphs):
-    def get_first_row(temp):
-        def get_div():
+def generate_tab_year(graphs): 
+    def get_div():
             text = f"""
             <div>
             <h4>Network Properties</h4>
@@ -78,26 +78,41 @@ def generate_tab_year(graphs):
             div = Div(text=text, width=500, height=100)
             return div
 
+    def get_first_row(temp):       
+
         year_collab = generate_year_graph(
             temp.graph_year, f"Collaborations in {i}")
         degree_distribution = get_degree_distribution(
             temp.graph_year, "Degree Distribution")
-        return row([year_collab, degree_distribution, get_div()])
+        return row([year_collab, degree_distribution])
 
     text = """<h3>Collaboration over years</h3><hr/>"""
     div = Div(text=text, width=500, height=100)
     tabs = []
 
-    def get_second_row(temp):
-        collaboration = generate_year_graph(temp.graph_previous_years, f"Collaborations from 2000 to {i}")
-        giant_component = generate_giant_componnet(temp.year_info['connected_components'][0])        
-        return row([collaboration, giant_component])
+    def connect_tabs(temp):
+        components = []
+        for i in temp.year_info['connected_components']:
+            components.append(generate_giant_componnet(i) )
+        lay = []
+        temp = []
+        for i in range(len(components)):   
+            temp.append(components[i])         
+            if (i+1)%3 == 0:
+                lay.append(temp)
+                temp = []           
+        giant_component = layout(lay)
+        return giant_component
+
 
     for i in range(2000, 2021):
         temp = graphs[i]
-        row_1 = get_first_row(temp)
-        row_2 = get_second_row(temp)
-        grid = layout([row_1, row_2 ])
+        stuff = []
+        year_tab = Panel(child = get_first_row(temp), title = "Year Analysis")
+        info_tab = Panel(child = get_div(), title = "Information")
+        connected_component_tab = Panel(child = connect_tabs(temp), title = "Connected Componnents")    
+        till_now  = Panel(child =  generate_year_graph(temp.graph_previous_years, f"Collaborations from 2000 to {i}"), title = "Cummalative Collaboration")  
+        grid = Tabs(tabs=[year_tab, info_tab, connected_component_tab])
         tabs.append(Panel(child=grid, title=str(i)))
 
     tab = Tabs(tabs=tabs)
@@ -111,7 +126,6 @@ def generate_faculty(Faculty, name):
 
         HOVER_TOOLTIPS = [("Faculty", "@index")]
         
-
         plot = figure(tooltips=HOVER_TOOLTIPS,tools="pan,wheel_zoom,save,reset", active_scroll='wheel_zoom', toolbar_location = None,
                       x_range=Range1d(-11.1, 11.1), y_range=Range1d(-11.1, 11.1), title=str(title), plot_width=400, plot_height=400)
            
@@ -152,11 +166,43 @@ def startingHtml():
     div = Div(text=text, width=500, height=100)
     return div
 
+def overall_information_graphs(Year_Graph ):
+    def get_average_degree_plot(title = "Average Degree"):
+        l1 = []
+        l2 = []
+        for i in range(2000,2021):
+            l1.append(i)
+            l2.append(Year_Graph[i].year_info['average_degree'])
+        p = figure(title=title, plot_width=400, plot_height=400,
+               y_axis_type="log", x_axis_type="log", toolbar_location = None)
+        p.vbar(x = l1, top = l2, line_width=2)
+        return p
+
+    def get_average_clustering_plot(title = "Average Clustering Coefficient"):
+        l1 = []
+        l2 = []
+        for i in range(2000,2021):
+            l1.append(i)
+            l2.append(Year_Graph[i].year_info['average_clustering_coefficient'])
+        p = figure(title=title, plot_width=400, plot_height=400,
+               y_axis_type="log", x_axis_type="log", toolbar_location = None)
+        p.vbar(x = l1, top = l2, line_width=2)
+        return p
+
+    t1 = get_average_degree_plot()
+    t2 = get_average_clustering_plot()
+    return layout([t1,t2])
+    
 
 def show_html(Year_Graph, name=None, Faculty=None):
     years_scse = generate_tab_year(Year_Graph)
     faculty = generate_faculty(Faculty, name)
-    tab = Tabs(tabs = [Panel(child = years_scse, title = "\tYear Wise Analysis\t"), Panel(child = faculty, title = "\tFaculty Collaborations\t")])
+    properties = overall_information_graphs(Year_Graph)
+    panel_list = []
+    panel_list.append(Panel(child = years_scse, title = "Year Wise Analysis"))
+    panel_list.append(Panel(child = faculty, title = "Faculty Collaborations"))
+    panel_list.append(Panel(child = properties, title = "Analysis"))
+    tab = Tabs(tabs = panel_list)
     l = [startingHtml(), tab]
     disp = layout(l)
     show(disp)
